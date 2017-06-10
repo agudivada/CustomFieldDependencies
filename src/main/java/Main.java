b868502715c5ad8d901ec70a8b465a55bfdda2ad
Anuj Kumar Gudivada <agudivada@agudivada-ltm3.internal.salesforce.com>
2017-06-10 14:00:26 -0400

    Port line added to main.java after load to solve errorWeb process failed to bind to  within 90 seconds of launch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import static spark.Spark.*;
 public class Main {
 	public static void main(String[] args) {
 
+		port(Integer.valueOf(System.getenv("PORT")));
+
 		//get("/", (request, response) -> {
 		
 		//System.out.println("Hello World");
