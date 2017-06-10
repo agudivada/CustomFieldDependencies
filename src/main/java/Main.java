2a4dc5186ea622dc1fde058d906b0eb6b9ad8754
Anuj Kumar Gudivada <agudivada@agudivada-ltm3.internal.salesforce.com>
2017-06-10 13:16:57 -0400

    chnages adding Spark library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import static spark.Spark.*;
 
 public class Main {
 	public static void main(String[] args) {
+
+		get("/", (request, response) -> {
 		
 		System.out.println("Hello World");
-		
+		}
 	}
 	
 }
